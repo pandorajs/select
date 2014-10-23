36d86cb88609815a2d9c54a98db9e4a7912adb6c
lynzz <lynzz168@gmail.com>
2014-10-23 10:53:54 +0800

    update test spec

--- test/select-spec.js
+++ test/select-spec.js
@@ -93,7 +93,7 @@ define(function(require, exports, module) {
           }
         });
 
-        expect(select.role('single-text').text()).to.be(select.option('defaultText'));
+        expect(select.role('item').eq(0).text()).to.be(select.option('defaultText'));
         expect(select.role('item').length).to.be(4);
         expect(select.role('item').first().text()).to.be(select.option('defaultText'));
       });
@@ -155,6 +155,92 @@ define(function(require, exports, module) {
 
     });
 
+    describe('Method', function() {
+      it('keyBack', function () {
+        select = new Select({
+          field: '#test',
+          search: true,
+          load: function(callback) {
+            callback([
+              {value:'0', text:'blue template'},
+              {value:'1', text:'red template'},
+              {value:'2', text:'green template'}
+            ]);
+          }
+        });
+
+        select.role('select').trigger('click');
+        select.keyBack();
+
+        expect(select.role('item').length).to.be(3);
+        expect(select.role('single-text').is(':visible')).to.be(false);
+        expect(select.value).to.be(null);
+      });
+
+      it('clearValue', function () {
+        select = new Select({
+          field: '#test',
+          search: true,
+          model: [
+            {value:'0', text:'blue template'},
+            {value:'1', text:'red template'},
+            {value:'2', text:'green template'}
+          ]
+        });
+
+        select.role('select').trigger('click');
+        select.role('item').eq(1).trigger('click');
+
+        select.clearValue();
+        expect(select.data('select')[0].selected).to.be(false);
+        expect(select.value).to.be(null);
+        expect(select.searchInput.val()).to.be('');
+      });
+
+      it('showDropdown and hideDropdown', function () {
+        select = new Select({
+          field: '#test',
+          search: true,
+          model: [
+            {value:'0', text:'blue template'},
+            {value:'1', text:'red template'},
+            {value:'2', text:'green template'}
+          ]
+        });
+
+        select.showDropdown();
+        expect(select.role('dropdown').is(':visible')).to.be(true);
+        expect(select.showSelect).to.be(true);
+
+        select.hideDropdown();
+        expect(select.role('dropdown').is(':visible')).to.be(false);
+        expect(select.showSelect).to.be(false);
+      });
+
+      it('setValue', function () {
+        select = new Select({
+          field: '#test',
+          hasOptionAll: true,
+          model: [
+            {value:'0', text:'blue template'},
+            {value:'1', text:'red template'},
+            {value:'2', text:'green template'}
+          ]
+        });
+
+        select.setValue('');
+        expect(select.field.val()).to.be('');
+        expect(select.value).to.be(null);
+
+        select.setValue('1');
+        expect(select.field.val()).to.be('1');
+
+        select.setValue('2');
+        expect(select.field.val()).to.be('2');
+      });
+
+    });
+
     describe('search with sifter', function() {
       it('show search input', function () {
         select = new Select({
@@ -198,25 +284,6 @@ define(function(require, exports, module) {
         expect(select.role('empty').text()).to.eql(select.option('sifterOptions/emptyTemplate'));
       });
 
-      it('clear value', function () {
-        select = new Select({
-          field: '#test',
-          search: true,
-          model: [
-            {value:'0', text:'blue template'},
-            {value:'1', text:'red template'},
-            {value:'2', text:'green template'}
-          ]
-        });
-
-        select.role('select').trigger('click');
-        select.role('item').eq(1).trigger('click');
-
-        select.clearValue();
-        expect(select.data('select')[0].selected).to.be(false);
-        expect(select.value).to.be(null);
-        expect(select.searchInput.val()).to.be('');
-      });
 
       it('async load and open search', function () {
         select = new Select({
@@ -231,7 +298,35 @@ define(function(require, exports, module) {
           }
         });
 
-        expect(select.role('item').length).to.be(3);
+        select.keyBack();
+
+        select.searchInput.val('red');
+        select.search();
+        expect(select.role('item').length).to.be(1);
+        expect(select.role('item').eq(0).data('value')).to.be(1);
+
+
+      });
+
+      it('select item with search', function () {
+        select = new Select({
+          field: '#test',
+          search: true,
+          load: function(callback) {
+            callback([
+              {value:'0', text:'blue template'},
+              {value:'1', text:'red template'},
+              {value:'2', text:'green template'}
+            ]);
+          }
+        });
+
+        select.keyBack();
+        select.searchInput.val('red');
+        select.search();
+
+        select.setValue('1');
+        expect(select.field.val()).to.be('1');
       });
 
     });
